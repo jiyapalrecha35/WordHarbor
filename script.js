0d87a29cb28eb39c2ca61e47488475b8ea4fb684
Jiya <jiyapalrecha35@gmail.com>
2023-08-15 16:27:00 +0530

    dict

---
+++ script.js
@@ -0,0 +1,31 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const btn = document.getElementById("searchBtn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inpWord").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            result.innerHTML = `
+            <div class="word">
+                <h3>${inpWord}</h3>
+            </div>
+            <div class="detail">
+                <p>${data[0].meanings[0].partOfSpeech}</p>
+                <p>/${data[0].phonetic}/</p>
+            </div>
+            <p class="wordMeaning">
+                ${data[0].meanings[0].definitions[0].definition}
+            </p>
+            <p class="wordExample">
+                ${data[0].meanings[0].definitions[0].example || ""}
+            </p>
+            `;
+
+        })
+        .catch(() => {
+            result.innerHTML = `<h4 class="error">Couldn't Find The Word</h4>`;
+        });
+});
+
